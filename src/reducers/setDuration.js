224eacfdc7e98cb92aa7869f3b50d76cf7a87703
Joshua Blum <83979765+joshua-blum@users.noreply.github.com>
2021-11-07 23:44:51 -0800

    Updated setFocusDuration reducer
    to respond to action payload

--- src/reducers/setDuration.js
+++ src/reducers/setDuration.js
@@ -1,11 +1,9 @@
 export const setFocusDuration = (state = 25, {type, payload = null}) => {
     switch(type){
         case 'INCREMENT_FOCUS':
-            return state+payload;
-            //return state+payload > 60 ? state: state+payload;
+            return state+payload > 60 ? state: state+payload;
         case 'DECREMENT_FOCUS':
-            return state-payload;
-            //return state-payload < 5 ? state: state-payload;
+            return state-payload < 5 ? state: state-payload;
         default:
             return state;
     }
